48d7620379eeab619c09b5a7020bf6decf3b6dc6
Matteo <matteo.violani@studio.unibo.it>
2023-03-29 20:05:58 +0200

    feat(qrcode_nav_section): added animation icon

--- lib/views/qrcode_nav_section.dart
+++ lib/views/qrcode_nav_section.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
+const double iconSize = 160;
+const double qrcodeSize = 100;
+
+Widget hiddenCode = Image.asset(
+  "/icons/push-data-icon.png",
+  height: iconSize,
+  alignment: Alignment.bottomCenter,
+);
+
+Widget showingCode = Stack(
+  alignment: Alignment.center,
+  children: [
+    Image.asset(
+      "/icons/empty-vase.png",
+      height: iconSize,
+      alignment: Alignment.bottomCenter,
+    ),
+    Image.asset(
+      "/icons/demo-qr.png",
+      height: qrcodeSize,
+      alignment: Alignment.bottomCenter,
+    ),
+  ],
+);
+
 class QrCodeNavSection extends StatefulWidget {
   const QrCodeNavSection({super.key});
 
@@ -8,9 +33,47 @@ class QrCodeNavSection extends StatefulWidget {
 }
 
 class _QrCodeNavSectionState extends State<QrCodeNavSection> {
+  bool _showQr = false;
+  Widget _currentIcon = hiddenCode;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return SizedBox(
+      height: 300,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          GestureDetector(
+            onTap: () => _updateQrIcon(),
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 400),
+              transitionBuilder: (Widget child, Animation<double> animation) =>
+                  ScaleTransition(
+                scale: animation,
+                child: child,
+              ),
+              child: _currentIcon,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              _showQr
+                  ? "Apri l’app, vai nel profilo e scansiona il qr"
+                  : "Deposita progressi APP",
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
+              overflow: TextOverflow.ellipsis,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _updateQrIcon() {
+    setState(() {
+      _showQr = !_showQr;
+      _currentIcon = _showQr ? showingCode : hiddenCode;
+    });
   }
 }
