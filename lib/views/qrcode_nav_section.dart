cf5b82051200c5c8d4896296893c91fb5b56c9eb
Matteo <matteo.violani@studio.unibo.it>
2023-03-29 11:14:58 +0200

    feat(qrcode_nav_section): add qr section in menu

---
+++ lib/views/qrcode_nav_section.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QrCodeNavSection extends StatefulWidget {
+  const QrCodeNavSection({super.key});
+
+  @override
+  State<QrCodeNavSection> createState() => _QrCodeNavSectionState();
+}
+
+class _QrCodeNavSectionState extends State<QrCodeNavSection> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
