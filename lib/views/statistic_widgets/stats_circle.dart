1a6d49babdab307650154631fb424f6c75755289
Matteo <matteo.violani@studio.unibo.it>
2023-03-30 15:48:41 +0200

    firx(stats_circle): fixed not working tap action

--- lib/views/statistic_widgets/stats_circle.dart
+++ lib/views/statistic_widgets/stats_circle.dart
@@ -8,26 +8,43 @@ class StatsCircle extends StatefulWidget {
 }
 
 class _StatsCircleState extends State<StatsCircle> {
+  //TODO: set this var passing from external
+  var color = const Color.fromRGBO(73, 166, 31, 1);
+  double size = 230;
+  double strokeWidht = 80; //TODO: check: it must be less than size
+  Widget icon = Image.asset("/icons/statistics/forest.png", height: 80);
+  String label = "ALBERI PIANTATI";
+
+  bool infoShowed = false;
+  double radius = 230;
+
   @override
   Widget build(BuildContext context) {
-    //TODO: set this var passing from external
-    var _color = const Color.fromRGBO(73, 166, 31, 1);
-    double _size = 225;
-    Widget _icon = Image.asset("/icons/statistics/forest.png", height: 80);
-    String _label = "ALBERI PIANTATI";
-
-    return Column(
-      children: [
-        _makeLabel(_label),
-        Stack(
-          alignment: Alignment.center,
+    return InkWell(
+      hoverColor: _getSecondaryColor(color),
+      borderRadius: BorderRadius.all(Radius.circular(radius)),
+      onTap: () {
+        setState(() {
+          infoShowed = !infoShowed;
+          radius = infoShowed ? 20.0 : size;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
           children: [
-            _makeCircle(_color, _size),
-            _makeCircle(Colors.white, 142),
-            _icon,
+            _makeLabel(label),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                _makeCircle(color, size, radius),
+                _makeCircle(Colors.white, size - strokeWidht, radius),
+                icon,
+              ],
+            )
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 }
@@ -52,13 +69,22 @@ Widget _makeLabel(String label) {
   );
 }
 
-Widget _makeCircle(Color color, double size) {
+Widget _makeCircle(Color color, double size, [double? radius]) {
   return Container(
     width: size,
     height: size,
     decoration: BoxDecoration(
       color: color, // border color
-      shape: BoxShape.circle,
+      borderRadius: BorderRadius.all(Radius.circular(radius ?? size)),
     ),
+  );
+}
+
+Color _getSecondaryColor(Color color) {
+  return Color.fromRGBO(
+    color.red,
+    color.green,
+    color.blue,
+    color.opacity - 0.8,
   );
 }
