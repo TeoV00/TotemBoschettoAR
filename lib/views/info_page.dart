951784b5b4c45c2b18235fd343be965c8c9f709f
Matteo <matteo.violani@studio.unibo.it>
2023-03-31 13:47:03 +0200

    feat(info_page): added infopage and utils

---
+++ lib/views/info_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:totem_boschetto/views/text_style_infopage.dart';
+
+var sections = <Widget>[
+  makeTitle("ReMade"),
+  makeParagraph(
+      "Il progetto che collega il risparmio della (processo di dematerializzazione) alla piantumazione proporzionale di alberi"),
+  Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      Image.asset("images/remade.png", fit: BoxFit.fitWidth),
+      Expanded(
+        child: Column(
+          // align text on left
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            makeHeader("OBIETTIVO"),
+            makeParagraph(
+              "Attraverso il Progetto ReMade (REplanting for Monitoring Accomplishment of DEmaterialization) l'Ateneo vuole potenziare l'effetto del risparmio di carta prodotto dalla digitalizzazione dei processi amministrativi e di comunicazione. Ad esempio, la stampa delle brochure di AlmaOrienta consumava 13 tonnellate di carta all'anno. Questo risparmio di carta (processo di dematerializzazione) si tradurrà nella piantumazione proporzionale di alberi in giardini dedicati allo scopo. ",
+            ),
+            makeHeader("ATTIVITÀ"),
+            makeParagraph(
+                "Oltre alla piantumazione di nuove alberature, i risultati virtuali (meno carta) e reali (più alberi) saranno diffusi attraverso una web app per affiancare, all'effetto sull'ambiente, un aumento di consapevolezza e competenza da parte della comunità Unibo."),
+          ],
+        ),
+      ),
+    ],
+  ),
+  makeHeader("Render"),
+  Image.asset("/images/render_cesena.jpeg"),
+  Padding(
+    padding: const EdgeInsets.all(40.0),
+    child: Column(
+      children: [
+        makeParagraph(
+            "Con questo progetto L'università di Bologna constribuisce algi obbiettivi dell' Agenda ONU 2030"),
+        Image.asset("/images/sdg_remade.png")
+      ],
+    ),
+  ),
+];
+
+class InfoPage extends StatelessWidget {
+  const InfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(30),
+      scrollDirection: Axis.vertical,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [...sections],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/text_style_infopage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const titleStyle = TextStyle(fontSize: 60, fontWeight: FontWeight.w900);
+const headerStyle = TextStyle(fontSize: 40, fontWeight: FontWeight.w600);
+const paragraphStyle = TextStyle(fontSize: 30);
+
+Text makeTitle(String titleText) {
+  return _makeText(titleText, titleStyle);
+}
+
+Widget makeHeader(String headerText) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 40, bottom: 5),
+    child: _makeText(headerText, headerStyle),
+  );
+}
+
+Text makeParagraph(String paragraphText) {
+  return _makeText(paragraphText, paragraphStyle);
+}
+
+Text _makeText(String text, TextStyle style) {
+  return Text(
+    text,
+    style: style,
+  );
+}
