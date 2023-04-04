77402ab39c6f3393d9fc7a41003b9b50fa3d16f4
Matteo <matteo.violani@studio.unibo.it>
2023-04-04 19:51:50 +0200

    feat(level_shelf): new widget for level rapresentation

--- lib/views/home_page/details_content.dart
+++ lib/views/home_page/details_content.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:totem_boschetto/views/home_page/home_utils.dart';
+import 'package:totem_boschetto/views/home_page/level_shelf.dart';
 
 class DetailsContent extends StatelessWidget {
   final String nickname;
@@ -37,52 +37,7 @@ class DetailsContent extends StatelessWidget {
             )
           ],
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                SvgPicture.asset(
-                  "$iconPath/shelf.svg",
-                  height: 52,
-                  width: 200,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 35),
-                  child:
-                      SvgPicture.asset("$iconLevelPath$level.svg", width: 120),
-                ),
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 8,
-                        offset: Offset(0, 3),
-                        color: Color.fromRGBO(0, 0, 0, 0.3),
-                      )
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 2.0,
-                      horizontal: 15,
-                    ),
-                    child: Text(
-                      "Lv. $level",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 25,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        )
+        LevelShelf(level: level)
       ]),
     );
   }
---
+++ lib/views/home_page/level_shelf.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:totem_boschetto/views/home_page/home_utils.dart';
+
+class LevelShelf extends StatelessWidget {
+  const LevelShelf({
+    super.key,
+    required this.level,
+  });
+
+  final int level;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            SvgPicture.asset(
+              "$iconPath/shelf.svg",
+              height: 52,
+              width: 200,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 35),
+              child: SvgPicture.asset("$iconLevelPath$level.svg", width: 120),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 8,
+                    offset: Offset(0, 3),
+                    color: Color.fromRGBO(0, 0, 0, 0.3),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 2.0,
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "Lv. $level",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
