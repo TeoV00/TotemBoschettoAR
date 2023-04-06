808f4662b18ded48bd76d21559d503a423cdcfe5
Matteo <matteo.violani@studio.unibo.it>
2023-04-06 12:29:29 +0200

    fix(stats_): added other counters

--- lib/views/stats_page.dart
+++ lib/views/stats_page.dart
@@ -10,7 +10,39 @@ var stat = <StatsCircle>[
     description:
         "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
     icon: Image.asset("${statIconPath}forest.png"),
-    value: '300', 
+    value: '300',
+  ),
+  StatsCircle(
+    color: const Color.fromRGBO(234, 156, 98, 1),
+    label: "PROGETTI UNIBO COINVOLTI",
+    description:
+        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
+    icon: Image.asset("${statIconPath}project.png"),
+    value: '30/40',
+  ),
+  StatsCircle(
+    color: const Color.fromRGBO(105, 93, 105, 1),
+    label: "CO2 EVITATA",
+    description:
+        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
+    icon: Image.asset("${statIconPath}co2.png"),
+    value: '300 Kg',
+  ),
+  StatsCircle(
+    color: const Color.fromRGBO(181, 206, 255, 1),
+    label: "CARTA RISPARMIATA",
+    description:
+        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
+    icon: Image.asset("${statIconPath}paper.png"),
+    value: '30/40',
+  ),
+  StatsCircle(
+    color: const Color.fromRGBO(254, 210, 164, 1),
+    label: "CONSAPEVOLEZZA ACQUISITA",
+    description:
+        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
+    icon: Image.asset("${statIconPath}awareness.png"),
+    value: '30%',
   ),
 ];
 
