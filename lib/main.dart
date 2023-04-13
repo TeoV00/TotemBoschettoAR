7ace3b229ae0744c730e3bd64b9367ec92bada05
Matteo <matteo.violani@studio.unibo.it>
2023-04-13 12:36:30 +0200

    feat(main,pubspec,firebase_option): added firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:totem_boschetto/views/navigation_menu/navigation_rail_menu.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
