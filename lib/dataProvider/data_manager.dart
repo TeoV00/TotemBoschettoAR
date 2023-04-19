c5acc6cedcb3c972fba5f71b675f682eb8b17e09
Matteo <matteo.violani@studio.unibo.it>
2023-04-19 11:18:22 +0200

    fix(data_manager, firebase_provider): removed data_manger class and renamed firebase_provider a data_manager

--- lib/dataProvider/data_manager.dart
+++ lib/dataProvider/data_manager.dart
@@ -1,19 +1,35 @@
-import 'package:totem_boschetto/DataProvider/firebase_provider.dart';
+import 'dart:developer';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:totem_boschetto/model/share_data_model.dart';
 
 class DataManager {
-  late FirebaseProvider _firebaseProvider;
+  final String _totemId = "ces_remade";
+  late final DatabaseReference dbRef;
 
   DataManager() {
-    _firebaseProvider = FirebaseProvider();
+    dbRef = FirebaseDatabase.instance.ref("totems/$_totemId");
+    dbRef.onChildAdded.listen((event) {
+      log("Dati utente caricati correttamente ");
+    });
+    dbRef.onChildChanged.listen((event) {
+      log("Dati caricati correttamente ");
+    });
   }
 
-  Future<List<SharedData>> getTotemData() {
-    return _firebaseProvider.getTotemData(totemId: getCurrentTotemId());
+  Future<List<SharedData>> getTotemData() async {
+    List<SharedData> sharedUserData = [];
+    DataSnapshot snap = await dbRef.get();
+
+    if (snap.exists) {
+      sharedUserData = snap.children
+          .map((e) =>
+              SharedData.fromMap(Map<String, dynamic>.from(e.value as Map)))
+          .toList();
+    }
+    return sharedUserData;
   }
 
   String getCurrentTotemId() {
-    //TODO: make that editable and save in preferences
-    return "ces_remade";
+    return _totemId;
   }
 }
