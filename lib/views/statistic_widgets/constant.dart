d2d34584a12ccc8aebf5e6199311471564e1266b
Matteo <matteo.violani@studio.unibo.it>
2023-04-19 14:47:34 +0200

    fix(stats_page): refactoring stats page code  getting values from datamanager

---
+++ lib/views/statistic_widgets/constant.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:totem_boschetto/views/stats_page.dart';
+
+// COLORS
+const treeColor = Color.fromRGBO(73, 166, 31, 1);
+const projColor = Color.fromRGBO(234, 156, 98, 1);
+const co2Color = Color.fromRGBO(105, 93, 105, 1);
+const paperColor = Color.fromRGBO(181, 206, 255, 1);
+const awarenessColor = Color.fromRGBO(254, 210, 164, 1);
+
+// ICONS
+final treeIcon = Image.asset("${statIconPath}forest.png");
+final projIcon = Image.asset("${statIconPath}project.png");
+final co2Icon = Image.asset("${statIconPath}co2.png");
+final paperIcon = Image.asset("${statIconPath}paper.png");
+final awarenessIcon = Image.asset("${statIconPath}awareness.png");
+
+// LABELS
+const treeLabel = 'ALBERI PIANTATI';
+const projLabel = 'PROGETTI COINVOLTI';
+const co2Label = 'CO2 EVITATA';
+const paperLabel = 'CARTA RISPARMIATA';
+const awarenessLabel = 'CONSAPEVOLEZZA';
+
+// DESCRIPTIONS
+const treeDescr = '';
+const projDescr = '';
+const co2Descr = '';
+const paperDescr = '';
+const awarenessDescr = '';
--- lib/views/stats_page.dart
+++ lib/views/stats_page.dart
@@ -1,61 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:totem_boschetto/dataProvider/data_manager.dart';
+import 'package:totem_boschetto/views/statistic_widgets/constant.dart';
 import 'package:totem_boschetto/views/statistic_widgets/stats_circle.dart';
 
 const statIconPath = "/icons/statistics/";
 
-var stat = <StatsCircle>[
-  StatsCircle(
-    color: const Color.fromRGBO(73, 166, 31, 1),
-    label: "ALBERI PIANTATI",
-    description:
-        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
-    icon: Image.asset("${statIconPath}forest.png"),
-    value: '300',
-  ),
-  StatsCircle(
-    color: const Color.fromRGBO(234, 156, 98, 1),
-    label: "PROGETTI UNIBO COINVOLTI",
-    description:
-        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
-    icon: Image.asset("${statIconPath}project.png"),
-    value: '30/40',
-  ),
-  StatsCircle(
-    color: const Color.fromRGBO(105, 93, 105, 1),
-    label: "CO2 EVITATA",
-    description:
-        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
-    icon: Image.asset("${statIconPath}co2.png"),
-    value: '300 Kg',
-  ),
-  StatsCircle(
-    color: const Color.fromRGBO(181, 206, 255, 1),
-    label: "CARTA RISPARMIATA",
-    description:
-        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
-    icon: Image.asset("${statIconPath}paper.png"),
-    value: '30/40',
-  ),
-  StatsCircle(
-    color: const Color.fromRGBO(254, 210, 164, 1),
-    label: "CONSAPEVOLEZZA ACQUISITA",
-    description:
-        "lore ipsoidninic sdnkn csdlak ontorllare perche se aumento non si ingrandisce il cerchio var iconSize = circleSize - 150.0; ",
-    icon: Image.asset("${statIconPath}awareness.png"),
-    value: '30%',
-  ),
-];
-
 class StatisticPage extends StatelessWidget {
-  const StatisticPage({super.key});
+  final DataManager dataManager;
+  const StatisticPage({super.key, required this.dataManager});
 
   @override
   Widget build(BuildContext context) {
+    var statInfo = <StatsCircle>[
+      StatsCircle(
+        color: treeColor,
+        label: treeLabel,
+        description: treeDescr,
+        icon: treeIcon,
+        value: dataManager.getPlantedTree(),
+      ),
+      StatsCircle(
+        color: projColor,
+        label: projLabel,
+        description: projDescr,
+        icon: projIcon,
+        value: dataManager.getProjectInvolved(),
+      ),
+      StatsCircle(
+        color: co2Color,
+        label: co2Label,
+        description: co2Descr,
+        icon: co2Icon,
+        value: dataManager.getAvoidedCo2(),
+      ),
+      StatsCircle(
+        color: paperColor,
+        label: paperLabel,
+        description: paperDescr,
+        icon: paperIcon,
+        value: dataManager.getSavedPaper(),
+      ),
+      StatsCircle(
+        color: awarenessColor,
+        label: awarenessLabel,
+        description: awarenessDescr,
+        icon: awarenessIcon,
+        value: dataManager.getAwareness(),
+      ),
+    ];
+
     return GridView.count(
-      crossAxisCount: 3,
-      mainAxisSpacing: 60,
-      crossAxisSpacing: 60,
-      children: [...stat],
-    );
+        crossAxisCount: 3,
+        mainAxisSpacing: 60,
+        crossAxisSpacing: 60,
+        children: statInfo);
   }
 }
