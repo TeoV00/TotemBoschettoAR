4d1d57f4d276d63d475eb50d892be25b1480b7a4
Matteo <matteo.violani@studio.unibo.it>
2023-04-20 17:42:11 +0200

    feat(info_grid_tile): added grid tile

---
+++ lib/views/info_page/info_grid_tile.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+const labelTileStyle = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+);
+
+const hintStyle = TextStyle(
+  fontSize: 25,
+);
+
+class InfoGridTile extends StatelessWidget {
+  final String label;
+  final Widget? child;
+  final String? hintText;
+  final Color? color;
+  final double? cellCountWidth;
+
+  const InfoGridTile({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.color,
+    this.child,
+    this.cellCountWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color ?? const Color.fromRGBO(243, 243, 243, 1),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.25),
+            blurRadius: 4,
+            offset: Offset(0, 4),
+          )
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              label,
+              style: labelTileStyle,
+            ),
+            if (child != null) ...[child!],
+            if (hintText != null) ...[Text(hintText!, style: hintStyle)],
+          ],
+        ),
+      ),
+    );
+  }
+}
