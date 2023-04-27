474337747fbf6bc203b518f1893abcd892b815aa
Matteo <matteo.violani@studio.unibo.it>
2023-04-27 18:30:28 +0200

    fix(info_page): tiles to show moved in new file

--- lib/views/info_page.dart
+++ lib/views/info_page.dart
@@ -1,64 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:totem_boschetto/constant_vars.dart';
 import 'package:totem_boschetto/views/common/grid_view_widget/grid_tile_view.dart';
-import 'package:totem_boschetto/views/info_page/info_tile.dart';
-
-var tiles = <GridTileItem>[
-  const InfoTile(
-    label: "Come è stata calcolata la $co2String a partire dai fogli di carta?",
-    hintText: "Tocca per scoprire di più",
-  ),
-  const InfoTile(
-    label: "Come sono stati calcolati i litri di benzina?",
-    hintText: "Tocca per scoprire di più",
-  ),
-  const InfoTile(
-    label: "Come è stato calcolata l'energia elettrica?",
-    hintText: "Tocca per scoprire di più",
-  ),
-  InfoTile(
-    cellCountWidth: 2,
-    label:
-        "Con questi progetti, l'Ateneo contribuisce agli obiettivi dell'agenda ONU 2030",
-    child: LayoutBuilder(builder: (context, constraint) {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "$imagePath/sdg_remade.png",
-            width: 600,
-          ),
-        ],
-      );
-    }),
-  ),
-  const InfoTile(
-    label: "ReMade",
-    hintText: "Tocca per scoprire di più",
-    child: Text(
-      "Il progetto che collega il risparmio della carta (processo di dematerializzazione) alla piantumazione proporzionale di alberi",
-      style: TextStyle(fontSize: 26, overflow: TextOverflow.fade),
-    ),
-  ),
-  const InfoTile(
-    label: "empty",
-    hintText: "Tocca per scoprire di più",
-    child: Text(
-      "Il progetto che collega il risparmio della carta (processo di dematerializzazione) alla piantumazione proporzionale di alberi",
-      style: TextStyle(fontSize: 26, overflow: TextOverflow.fade),
-    ),
-  ),
-  const InfoTile(
-    label: "empty",
-    hintText: "Tocca per scoprire di più",
-    child: Text(
-      "Il progetto che collega il risparmio della carta (processo di dematerializzazione) alla piantumazione proporzionale di alberi",
-      style: TextStyle(fontSize: 26, overflow: TextOverflow.fade),
-    ),
-  ),
-];
+import 'package:totem_boschetto/views/info_page/info_tile_data.dart';
 
 class InfoPage extends StatefulWidget {
   const InfoPage({super.key});
@@ -77,11 +19,8 @@ class _InfoPageState extends State<InfoPage> implements GridViewNotifier {
         GridTileView(
           colCount: 3,
           rowCount: 3,
-          tiles: tiles,
+          tiles: infoTiles,
           tileSpacing: 20.0,
-          onTileTap: () => setState(() {
-            log("tapped tile");
-          }),
           listener: this,
         ),
         if (detailsWidget != null) ...[
