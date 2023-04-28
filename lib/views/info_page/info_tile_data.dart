9aee2cdfb39895d666035be5dcc1e2bd90a8eefd
Matteo <matteo.violani@studio.unibo.it>
2023-04-28 18:09:48 +0200

    fix(info_tile_data): added remade proj info

--- lib/views/info_page/info_tile_data.dart
+++ lib/views/info_page/info_tile_data.dart
@@ -3,6 +3,7 @@ import 'package:totem_boschetto/constant_vars.dart';
 import 'package:totem_boschetto/views/common/grid_view_widget/grid_tile_view.dart';
 import 'package:totem_boschetto/views/info_page/info_const.dart';
 import 'package:totem_boschetto/views/info_page/info_tile.dart';
+import 'package:totem_boschetto/views/text_style_infopage.dart';
 
 final infoTiles = <GridTileItem>[
   makeInfoTile(0),
@@ -26,7 +27,43 @@ final infoTiles = <GridTileItem>[
 ];
 
 Widget makeRemadeProjectDetails() {
-  return Text("data");
+  return Column(
+    children: [
+      makeTitle("ReMade"),
+      makeParagraph(
+          "Il progetto che collega il risparmio della (processo di dematerializzazione) alla piantumazione proporzionale di alberi"),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset("images/remade.png", fit: BoxFit.fitWidth),
+          Expanded(
+            child: Column(
+              // align text on left
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                makeHeader("OBIETTIVO"),
+                makeParagraph(
+                  "Attraverso il Progetto ReMade (REplanting for Monitoring Accomplishment of DEmaterialization) l'Ateneo vuole potenziare l'effetto del risparmio di carta prodotto dalla digitalizzazione dei processi amministrativi e di comunicazione. Ad esempio, la stampa delle brochure di AlmaOrienta consumava 13 tonnellate di carta all'anno. Questo risparmio di carta (processo di dematerializzazione) si tradurrà nella piantumazione proporzionale di alberi in giardini dedicati allo scopo. ",
+                ),
+                makeHeader("ATTIVITÀ"),
+                makeParagraph(
+                    "Oltre alla piantumazione di nuove alberature, i risultati virtuali (meno carta) e reali (più alberi) saranno diffusi attraverso una web app per affiancare, all'effetto sull'ambiente, un aumento di consapevolezza e competenza da parte della comunità Unibo."),
+              ],
+            ),
+          ),
+        ],
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: Column(
+          children: [
+            makeHeader("Render"),
+            Image.asset("/images/render_cesena.jpeg", width: 800),
+          ],
+        ),
+      ),
+    ],
+  );
 }
 
 InfoTile makeInfoTile(int idxInfo) {
