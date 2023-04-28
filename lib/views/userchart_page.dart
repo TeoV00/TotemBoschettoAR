e08ed9e9e0dd36247f3d02eff3997effe3a513b3
Matteo <matteo.violani@studio.unibo.it>
2023-04-28 13:29:23 +0200

    fix(userchart_page): show chart list from data

--- lib/views/userchart_page.dart
+++ lib/views/userchart_page.dart
@@ -2,35 +2,110 @@ import 'package:flutter/widgets.dart';
 import 'package:totem_boschetto/constant_vars.dart';
 import 'package:totem_boschetto/dataProvider/data_manager.dart';
 
+const chartData = [
+  {
+    'nickname': 'Teo',
+    'pos': 1,
+  },
+  {
+    'nickname': 'Miki',
+    'pos': 2,
+  },
+  {
+    'nickname': 'Luca',
+    'pos': 3,
+  },
+  {
+    'nickname': 'Pippo',
+    'pos': 4,
+  },
+  {
+    'nickname': 'Luana',
+    'pos': 5,
+  },
+  {
+    'nickname': 'Manu',
+    'pos': 6,
+  },
+  {
+    'nickname': 'Manu',
+    'pos': 7,
+  },
+  {
+    'nickname': 'Manu',
+    'pos': 8,
+  }
+];
+
 class UserChartView extends StatelessWidget {
   final DataManager dataManager;
   const UserChartView({super.key, required this.dataManager});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Row(
-            children: const [
-              ChartItem(
-                nickname: "TeoC",
-                position: 1,
-              ),
-              ChartItem(
-                nickname: "TeoC",
-                position: 4,
-              ),
-              ChartItem(
-                nickname: "TeoC",
-                position: 2,
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  "Classifica Top 10",
+                  style: TextStyle(fontSize: 40),
+                ),
+              ],
+            ),
           ),
-        )
-      ],
+          makeListColumn(
+              itemCount: 5,
+              items: chartData
+                  .map((e) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ChartItem(
+                            nickname: e['nickname'] as String,
+                            position: e['pos'] as int),
+                      ))
+                  .toList()),
+        ],
+      ),
     );
   }
+}
+
+Widget makeListColumn({required int itemCount, required List<Widget> items}) {
+  var firstCol = items.sublist(1, itemCount + 1);
+  var secCol = items.sublist(itemCount + 1);
+  // secCol.removeAt(0);
+  // secCol.insert(0, items.elementAt(2));
+  // firstCol.removeAt(1);
+  // firstCol.insert(firstCol.length, items.elementAt(itemCount));
+
+  return Column(
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [items.first],
+        ),
+      ),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: firstCol,
+          ),
+          Column(
+            children: secCol,
+          )
+        ],
+      ),
+    ],
+  );
 }
 
 class ChartItem extends StatelessWidget {
