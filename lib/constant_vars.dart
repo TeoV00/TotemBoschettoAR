ba83997e008daa80680656fb39c7244d9cd83df1
Matteo <matteo.violani@studio.unibo.it>
2023-04-28 12:11:58 +0200

    fix(constants_vars): added box shadow

--- lib/constant_vars.dart
+++ lib/constant_vars.dart
@@ -5,5 +5,11 @@ const co2String = "CO\u2082";
 const mainColor = Color.fromARGB(255, 161, 204, 130);
 const secondColor = Color(0xFFD6EFC7);
 
+const boxShadowBottom = BoxShadow(
+  blurRadius: 4,
+  offset: Offset(0, 4),
+  color: Color.fromRGBO(0, 0, 0, 0.25),
+);
+
 const iconsPath = "/icons";
 const imagePath = "/images";
